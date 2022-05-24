a7b2801a9e123c6d67c3b7b42d41b174c52223bb
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-24 23:37:10 +0530

    Add array of objects in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+  posts = [
+    { 
+      title: 'Neat Tree', 
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    { 
+      title: 'Snowy Mountain', 
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain'
+    },
+    { 
+      title: 'Mountain Biking', 
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker',
+      content: 'I did some biking today'
+    }
+  ];
 }
