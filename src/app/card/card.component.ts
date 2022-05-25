c6961e2afbfd68d52bcb0e2ff7404d24838e52dc
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-05-25 23:12:14 +0530

    Add comment for setting up Input binding

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -16,3 +16,13 @@ export class CardComponent implements OnInit {
   }
 
 }
+
+/* 
+  Setting up Input Binding
+  1) In the parent component template, find where we create the child component
+  2) Decide on the property name that we want to use to communicate from the parent to the child
+  3) Add a new binding to the child component, specifying the data we want to pass down
+  4) In the child component's class file, add an input property. This tells the component
+  to expect the parent to provide the value for this property
+  5) In the child component's template file, reference that input property
+*/
